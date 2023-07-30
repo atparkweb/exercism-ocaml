f602ee526a92057b46f8c39355d8d1810e6fe6ed
A Park <atpark333@gmail.com>
2023-07-30 16:06:18 +0900

    Add solution

--- bob/bob.ml
+++ bob/bob.ml
@@ -1,2 +1,45 @@
-let response_for _ =
-    failwith "'response_for' is missing"
+let is_question str =
+  let str = String.trim str in
+  let str_len = String.length str in
+    match str_len with
+      | 0 -> false
+      | _ -> str.[str_len - 1] = '?'
+
+let is_alpha c =
+  (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')
+
+let remove_non_alpha str =
+  let filtered_chars = String.fold_left (fun acc c ->
+    if is_alpha c then acc ^ String.make 1 c else acc
+  ) "" str in
+  filtered_chars
+
+let is_all_uppercase str =
+  let str = remove_non_alpha str in
+  if String.length str = 0 then
+    false
+  else
+    let is_uppercase_char c =
+      Char.uppercase_ascii c = c && is_alpha c in
+    let rec check_chars idx =
+      if idx >= String.length str then
+        true
+      else
+        let current_char = str.[idx] in
+        if is_uppercase_char current_char then
+          check_chars (idx + 1)
+        else
+          false
+    in
+    check_chars 0
+
+let is_empty str = (String.trim str |> String.length) = 0
+
+let response_for str =
+   let results = [is_empty str; is_question str; is_all_uppercase str;] in
+   match results with
+   | [true; _; _]         -> "Fine. Be that way!"
+   | [false; true; true]  -> "Calm down, I know what I'm doing!"
+   | [false; true; false] -> "Sure."
+   | [false; false; true] -> "Whoa, chill out!"
+   | _                    -> "Whatever."
